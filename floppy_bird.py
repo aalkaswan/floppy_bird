9fd3c50e4c63c5dbc094bdda5585af4555c45369
aalkaswan <a.al-kaswan@student.tudelft.nl>
2019-11-12 13:06:52 +0100

    Add bottom
    Added moving bottom of screen.

--- floppy_bird.py
+++ floppy_bird.py
@@ -81,6 +81,7 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
+
 class Pipe:
     GAP = 200
     VEL = 5
@@ -124,6 +125,31 @@ class Pipe:
             return True
 
         return False
+
+
+class Base:
+    VEL = 5
+    WIDTH = BASE.get_width()
+    IMG = BASE
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
 
 
 def draw_window(win, bird):
